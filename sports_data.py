f3e68aabd45cfc2e5d6d493ad2deb354f2b9a0b8
Rodrigo Morales <rodrigomv29@gmail.com>
2025-01-29 11:38:13 -0500

    respnse object is not subscriptable error

--- sports_data.py
+++ sports_data.py
@@ -3,18 +3,16 @@ import os
 import json
 from dotenv import load_dotenv
 
-
-def main():
-
+if __name__ == "__main__":
     load_dotenv()
     api_key = os.getenv("SPORTS_DATA_API_KEY")
     url = "https://replay.sportsdata.io/api/v4/soccer/odds/json/alternatemarketgameoddsbydate/epl/2023-09-23?key="+api_key  # Add the URL for the GET request to sportsdata.io
     response = requests.get(url)
+    res_data = response.json()
     if response.status_code == 200:
         with open('response.json', 'w') as file:
+            print(response[0])
+            print('writing to file')
             file.write(response.text)
     else:
         print(f"Failed to retrieve data: {response.status_code}")
-
-if __name__ == "__main__":
-    main()
