4961751d161acc1bdaec1fe5d447f22c35dbeb5f
Rodrigo Morales <rodrigomv29@gmail.com>
2025-01-29 22:54:38 -0500

    helper functions to parse json

--- sports_data.py
+++ sports_data.py
@@ -1,7 +1,26 @@
+"""
+{"Season": 
+ "Week":
+ "DateTime":
+ "Status":
+ "AwayTeamId": 
+ "HomeTeamId": 
+ "AwayTeamName": "Nottingham Forest FC", "HomeTeamName": "Manchester City FC", "HomeTeamScore": 2, "AwayTeamScore": 0, "TotalScore": 2,
+"""
 import requests
 import os
 import json
 from dotenv import load_dotenv
+
+def get_game_title(home, away):
+    return home + " is facing " + away 
+
+def get_odds(res, team):
+    home_money_line = res["AlternateMarketPregameOdds"][0]["HomeMoneyLine"]
+    away_money_line = res["AlternateMarketPregameOdds"][0]["AwayMoneyLine"]
+    home_money_int = int(home_money_line)
+    away_money_int = int(away_money_line)
+    return max(home_money_int, away_money_int)
 
 if __name__ == "__main__":
     load_dotenv()
@@ -16,9 +35,13 @@ if __name__ == "__main__":
     res_data[0].pop('GlobalGameId')
     res_data[0].pop('GlobalAwayTeamId')
     res_data[0].pop('GlobalHomeTeamId')
+
+
     if response.status_code == 200:
         with open('response.json', 'w') as file:
             print('writing to file')
             file.write(str(res_data))
     else:
         print(f"Failed to retrieve data: {response.status_code}")
+
+
