e0677c20bb2ac707e2240f1d1ad37237dc49092d
Rodrigo Morales <rodrigomv29@gmail.com>
2025-01-31 16:47:36 -0500

    new pregame odds class added

---
+++ AlternateMarketPregameOdds.py
@@ -0,0 +1,7 @@
+class AlternateMarketPregameOdds:
+    def __init__(self, game_odd_id, sportsbook, game_id, created, 
+                 updated, home_money_line, away_money_line, 
+                 draw_money_line, home_point_spread, away_point_spread,
+                 home_point_spread_payout, away_point_spread_payout,
+                 over_under, over_payout, under_payout, SportsbookId, SportsbookUrl, HomeTeamAsianHandicap, AwayTeamAsianHandicap, HomeTeamAsianHandicapPayout, AwayTeamAsianHandicapPayout, AsianTotal, AsianTotalOverPayout,AsianTotalUnderPayout, OddType):
+        pass
